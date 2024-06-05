0114aecae9f25006fe71c8507bb9823b14a6f0eb
Abraão Borel <abraaoborel@gmail.com>
2024-06-05 00:34:08 -0300

    alterando o enredo principal
    alterando tudo na verdade!
    talvez esse projeto crie outros projetos atraves desse o mais provavel!

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-# Menu
-print(f'''\033[36m
+# Antes de iniciar o jogo vamos configurar a linguagem
+# preciso solicitar do usuário uma linguagem, que ele vai querer jogar
+# como estou estudando Inglês, vai me ajudar bem também
+# traduzir todo o conteúdo de inglês para o português
+# Então vou crirar uma versão em portugues e uma versão em inglês
+
+msg = ' '
+while msg not in 'PI':
+    msg = str(input('''Escolha a linaguem do jogo:
+                      P ) Português
+                      I ) Inglês
+                      ''')).strip().upper()[0]
+    if msg == 'P':
+        print('Você escolheu jogar em Português.')
+    elif msg == 'I':
+        print('Você escolheu jogar em Inglês.')
+    else:
+        print('\033[31mOpção inválida! Digite uma opção válida!\033[m')
+
+
+opcao = ' '
+while opcao not in 'ABCD':
+    opcao = str(input('''\r 
+\033[36m
                                         ██                                    
                                         ██                                    
                                 ▒▒██████████████                              
@@ -34,4 +56,28 @@ print(f'''\033[36m
                               ██        ██                                    
                               ██▓▓    ░░██                                    
                               ░░████████                                      
-\033[m''')
+\033[m
+Digite uma das opções acima:
+                      ''')).strip().upper()[0]
+    if opcao == 'A':
+        print('Start')
+    elif opcao == 'B':
+        print('Mais sobre o jogo.')
+    elif opcao == 'C':
+        print('Creditos')
+    elif opcao == 'D':
+        ('Sair do jogo')
+        break
+    else:
+        print('\033[31mSelecione uma opção válida!\033[m')
+
+
+# a) start
+# b) More about this game
+# c) Credits
+# d) Exit
+
+# dentro de start
+# 1 - linguagem/ pt-br/ englsih
+# 2 - fases
+# 3 - 
---
+++ test.py
@@ -0,0 +1,115 @@
+import os
+from time import sleep
+from playsound import playsound
+import threading
+
+def play_sounds():
+    while True:
+        playsound('trovao1.mp3')
+        playsound('chuva1.mp3')
+        
+
+# Inicia a thread de som
+sound_thread = threading.Thread(target=play_sounds)
+sound_thread.daemon = True  # Permite que o programa encerre mesmo que a thread ainda esteja rodando
+sound_thread.start()
+
+
+def clear_screen_menu():
+#    Verifica o sistema operacional e executa o comando de limpeza de tela correspondente
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('\n' * 100)
+
+def clear_screen():
+#    Verifica o sistema operacional e executa o comando de limpeza de tela correspondente
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def menu_principal():
+  opcao = ' '
+  while opcao not in 'ABCD':
+      clear_screen()  # Limpa a tela
+      print('''\033[36m
+                                          ██                                    
+                                          ██                                    
+                                  ▒▒██████████████                              
+                            ██████▓▓████████████████████                        
+                        ████████████████████████████████████                    
+                      ██████████████████████████████████████████                
+                  ████████████████████████████████████████████████              
+              ▒▒████████████████████████████████████████████████████            
+            ██████████████████████████████████████████████████████████          
+          ▓▓████████████████████████████████████████████████████████████        
+        ░░████████▒▒██████████████████████████████▒▒██████████████████████      
+        ████████████████████████████████████████████████████████████████████    
+      ██████████████████████████████████████████████████████████████████████    
+      ██████▒▒████████████████████████████████████████████████████████████████  
+      ████████████████████████████████████████████████████████████████████████  
+    ██████████████████████████████████████████████████████████████████████████▒▒
+    ████████████████████████████████████████████████████████████████████████████
+    ████████████████████████████████████████████████████████████████████████████
+  ░░██████████████▒▒    ████████▒▒░░  ▒▒██████      ████▒▒        ▒▒██        ▒▒
+            ████░░        ██            ░░██          ██                        
+                                          ██                                    
+                                          ██       \033[1;97mby: Abraão Borel\033[m\033[36m   ██        
+                ▒▒                        ██                          ▒▒        
+              ████                        ██                                    
+  \033[1;97mMenu:\033[m                                   \033[36m██\033[m
+        \033[1;97m(a) Start\033[m                         \033[36m██\033[m
+        \033[1;97m(b) More about this game\033[m          \033[36m██\033[m
+        \033[1;97m(c) Credits\033[m                       \033[36m██\033[m
+        \033[1;97m(d) Exit\033[m\033[36m                          \033[36m██            ██            ▒▒                      
+                                          ██            ██                      
+                                ██        ██            ░░                      
+                                ██        ██                                    
+                                ██▓▓    ░░██                                    
+                                ░░████████                                      
+  \033[m
+      Digite uma das opções acima:
+      ''', end='')
+      opcao = str(input()).strip().upper()[0]
+      if opcao == 'A':
+          start()
+      elif opcao == 'B':
+          print('Mais sobre o jogo.')
+      elif opcao == 'C':
+          print('Creditos')
+      elif opcao == 'D':
+          print('Sair do jogo')
+          break
+      else:
+          print('\033[31mSelecione uma opção válida!\033[m')
+          input('Pressione Enter para continuar...')  # Pausa antes de limpar a tela
+
+def printar_rolando_o_texto(texto, delay=4):
+    linhas = texto.split('\n')
+    for linha in linhas:
+        clear_screen()
+        print(linha)
+        sleep(delay)  # Pausa entre as linhas para criar o efeito de rolagem
+
+def start():
+    printar_rolando_o_texto(texto1)
+
+def more_about_game():
+    pass
+
+def credits():
+    pass
+
+def exit():
+    pass
+
+
+texto1 = """
+Está caindo uma chuva bem forte.
+O barulho da chuva e dos trovões são bem altos.
+A noite está fria, e as ruas estão vazias.
+Você percebe que tem alguém te seguindo, e começa a andar mais rápido.
+Você olha para trás, mas vê apenas alguém com um guarda-chuva, acelerando os passos em direção a você.
+Ao olhar para trás você, repara que essa pessoa que está te seguindo está com uma faca na mão.
+Seu coração começa a bater mais forte, neste momento...
+Depois de perceber a situação você começa a andar mais rápido ainda!
+"""
+
+menu_principal()
